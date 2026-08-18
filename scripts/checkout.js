8fc66aa7e68e0d8f24e35c45fa3d20ec94701453
ferdaous <ferdaoushayoun@gmail.com>
2026-08-18 19:31:10 +0100

    save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
-import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderOrderSummary } from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
 renderOrderSummary();
-
-console.log(cart);
+renderPaymentSummary();
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'; //default export (only export one thing)
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 
 hello();
 const today = dayjs();
@@ -18,24 +18,12 @@ export function renderOrderSummary() {
 
       const productId = cartItem.productId;
 
-      let matchingProduct;
-
-      products.forEach((product) => {
-        if (product.id === productId) {
-          matchingProduct = product;
-        }
-      });
+      const matchingProduct = getProduct(productId);
 
       const deliveryOptionId = cartItem.deliveryOptionId;
 
-      let deliveryOption;
-
-      deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-          deliveryOption = option;
-        }
-      });
-
+      const deliveryOption = getDeliveryOption(deliveryOptionId);
+      
       const today = dayjs();
       const deliveryDate = today.add(deliveryOption.deliveryDays, 'day');
       const dateString = deliveryDate.format('dddd, MMMM D');
