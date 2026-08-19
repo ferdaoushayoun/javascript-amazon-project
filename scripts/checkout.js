089010f37047958cdfae8f21bf4ab18df6aa5bf8
ferdaous <ferdaoushayoun@gmail.com>
2026-08-19 23:05:18 +0100

    convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,49 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js'
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-loadProducts(() => {
+Promise.all([ // better Promise feature
+  new Promise((resolve) => { // similar to Jasmine done function
+  loadProducts(() => {
+    resolve('value1');
+  });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then((values) => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+//helps keep the code more flat
+// new Promise((resolve) => { // similar to Jasmine done function
+//   loadProducts(() => {
+//     resolve('value1');
+//   });
+
+// }).then((value) => {
+//   console.log(value);
+//   return new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   });
+
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
+
+// with the callback there are lot of nested code
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
